535c6f7d6389b5928905b024d837f1a98aa98ba2
Satish Fulwani <satish.fulwani63@gmail.com>
2021-06-07 18:43:23 +0530

    Modified Header

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ const getURLData = async (request_url) => {
     let structured_url = url_parse(request_url);
     let origin = structured_url.origin;
     let required_cookies;
-
+    let preflight_config = {
+      method: "get",
+      url: origin,
+    };
     //Performing pre-flight request
-    var pre_request = await axios.get(origin).then((res) => {
+    var pre_request = await axios(preflight_config).then((res) => {
       return res.headers;
     });
 
     //Setting required cookies
     try {
-      required_cookies = pre_request["set-cookie"].join(";");
+      let cookies = pre_request["set-cookie"].map(
+        (cookie) => cookie.split(";")[0]
+      );
+      console.log(cookies);
+      required_cookies = cookies.join(";");
     } catch {
       required_cookies = "";
     }
@@ -25,22 +32,44 @@ const getURLData = async (request_url) => {
     let config = {
       method: "get",
       url: request_url,
-      Cookie: required_cookies,
+      headers: {
+        Cookie: required_cookies,
+      },
     };
-    let response = await axios(config).then((res) => {
-      return res.data;
-    });
-    return response;
+
+    //Returning Data
+    let response = await axios(config).then((res) => res);
+    console.log(response.headers);
+    return {
+      response: response.data,
+      status: response.status,
+    };
   } catch (err) {
+    // console.log(err);
     console.log(`Aborting the process, found folowing error ${err}`);
-    throw new Error(err);
+    return {
+      response: "Something went wrong",
+      status: err.statusCode,
+    };
   }
 };
 
-handler = async (url) => {
-  console.log(`Running crawler for ${url}`);
-  let page_metadata = await getURLData(url);
-  console.log(page_metadata);
+exports.handler = async (event) => {
+  console.log(`Running crawler for ${event.url}`);
+  let page_metadata = await getURLData(event.url);
+  let { status, response } = page_metadata;
+  if ((status > 400) & (status < 500)) {
+    return {
+      statusCode: status,
+      body: JSON.stringify("The URL you provided does not exists"),
+    };
+  } else if (status > 500) {
+    return {
+      statusCode: status,
+      body: JSON.stringify("Something went wrong, please try after sometime"),
+    };
+  }
+  // console.log(response);
   // const response = {
   //   statusCode: 200,
   //   body: JSON.stringify("Hello from Lambda!"),
@@ -48,7 +77,7 @@ handler = async (url) => {
   // return response;
 };
 
-handler("https://www.amazon.in/gp/product/B07L4JDX4F/");
+// handler("https://www.amazon.in/gp/product/B07L4JDX4F/");
 // handler(
 //   "http://dl.flipkart.com/dl/mivi-roam2-5-w-bluetooth-speaker/p/itme55aaa9cc60bd?pid=ACCFXESJGGGT3DHC&cmpid=product.share.pp"
 // );
